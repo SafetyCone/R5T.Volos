c1fbbb5a2470d3fb22fcb1b963be72894636ef90
MinexAutomation <minex.automation@gmail.com>
2019-12-12 19:56:44 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Volos.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Volos.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

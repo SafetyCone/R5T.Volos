cf4fe76c969d8d238d2c9aefdfa7a1866fdb5903
MinexAutomation <minex.automation@gmail.com>
2019-12-12 20:02:48 -0800

    Added static CommandLine class with static New() method that gets an ICommandBuilderContext instance upon which commands can act.

--- source/R5T.Volos/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Volos
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Volos/Code/Classes/CommandLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using R5T.Heraklion;
+using R5T.Heraklion.Default;
+using R5T.Piraeus.StringBuilder;
+
+
+namespace R5T.Volos
+{
+    public static class CommandLine
+    {
+        public static ICommandBuilderContext<TContext> New<TContext>()
+        {
+            var commandBuilder = new StringBuilderBasedCommandBuilder();
+
+            var commandBuilderContext = new CommandBuilderContext<TContext>(commandBuilder);
+            return commandBuilderContext;
+        }
+    }
+}
